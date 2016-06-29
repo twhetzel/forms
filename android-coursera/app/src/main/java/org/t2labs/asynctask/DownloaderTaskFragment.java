26082a5112c7db5be5d635793ed5136f4c3c39b2
Trish Whetzel <plwhetzel@gmail.com>
2016-06-29 00:33:45 -0700

    update

--- android-coursera/app/src/main/java/org/t2labs/asynctask/DownloaderTaskFragment.java
+++
@@ -1,7 +0,0 @@
-package org.t2labs.asynctask;
-
-/**
- * Created by whetzel on 12/23/15.
- */
-public class DownloaderTaskFragment {
-}
--- android-coursera/app/src/main/java/org/t2labs/asynctask/SelectionListener.java
+++
@@ -1,7 +0,0 @@
-package org.t2labs.asynctask;
-
-/**
- * Created by whetzel on 12/22/15.
- */
-public class SelectionListener {
-}
